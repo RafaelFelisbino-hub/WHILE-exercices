b1bb19f6650de858d0440c9985f7d2ab7a1f93a9
RafaelFelisbino-hub <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-09 20:10:18 -0300

    Add files via upload

---
+++ Exercicio1WHILE.c
@@ -0,0 +1,22 @@
+//Ler um número inteiro n. Escrever a soma de todos os números de 1 até n.
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	int n, contador = 1,num = 0;
+
+	setlocale(LC_ALL, "Portuguese");
+	system("color 17");
+
+	printf("Digite o valor de N:  ");
+	scanf_s("%i", &n);
+
+	while (contador <= n) 
+	{
+		num += contador++;
+		printf("Os valores de N sao:  %i\n", num);
+	}
+	system("pause");
+}
